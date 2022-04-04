5d88affe2934225d41429e59da001d78c3fa4c94
MafujulHaquePlabon <mafujul15-3076@diu.edu.bd>
2022-04-05 05:10:26 +0600

    BarCharts added in dashboard

--- src/components/AreaCharts/AreaCharts.js
+++ src/components/AreaCharts/AreaCharts.js
@@ -6,7 +6,20 @@ const AreaCharts = ({dataItems}) => {
     console.log(dataItems)
     return (
         <div className='my-4 '>
-          
+          <h1 className="text-blue-700 font-semibold text-xl">Investment vs Revenue</h1>
+        <AreaChart
+          width={500}
+          height={400}
+          data={dataItems}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="month" />
+          <YAxis />
+          <Tooltip />
+          <Area  dataKey="investment"  />
+          <Area  dataKey="revenue"  />
+         
+        </AreaChart>
         </div>
       
     );
