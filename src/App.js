5072ddc39a96685ece05cb7b8cf746911719ca31
MafujulHaquePlabon <mafujul15-3076@diu.edu.bd>
2022-04-04 16:47:09 +0600

    public folder data.json file added recharts

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Route, Routes } from 'react-router';
 import Home from "./components/Home/Home"
 import Reviews from './components/Reviews/Reviews';
 import NotFound from './components/NotFound/NotFound'
+import Dashboard from './components/Dashboard/Dashboard';
 
 function App() {
   return (
@@ -14,6 +15,9 @@ function App() {
        <Route path="/" element={<Home></Home> }></Route>
        <Route path="/home" element={<Home></Home> }></Route>
        <Route path="/reviews" element={<Reviews></Reviews>}></Route>
+       <Route path="/dashboard" element={<Dashboard></Dashboard>}></Route>
+       {/* <Route path="/blogs" element={}></Route>
+       <Route path="/about" element={}></Route> */}
        <Route path="*" element={ <NotFound></NotFound> }></Route>
 
      </Routes>
--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -3,7 +3,13 @@ import { XIcon } from '@heroicons/react/solid'
 const NotFound = () => {
     return (
         <div>
-            <h1 className='text-green-700 text-2xl font-semibold flex justify-center'>Not Found 404 <span ><XIcon className='w-8 text-red-600 '></XIcon></span> </h1>
+           <div>
+           <h1 className='text-green-700 text-2xl font-semibold flex justify-center'>Page Not Found 
+             <span ><XIcon className='w-8 text-red-600 '></XIcon></span> </h1>
+            <div className='flex justify-center'>
+            <img  src="https://imgs.search.brave.com/jLLT4J9siXzYHiV8NzhDx8cxA49HVMxu2-2d5r7pxQQ/rs:fit:330:240:1/g:ce/aHR0cDovL2ltZzA4/LmRldmlhbnRhcnQu/bmV0Lzk2NDAvaS8y/MDEyLzIwMS8xLzAv/ZXJyb3JfNDA0X19m/aWxlX25vdF9mb3Vu/ZF9fYnlfbW9ydGlt/ZXJtY21pcmVzdGlu/a3MtZDU4MGUxay5w/bmc" alt="" />
+            </div>
+           </div>
         </div>
     );
 };
