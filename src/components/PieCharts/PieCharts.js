95cbe09f04b33b7eef240517f04b91b7bf968f15
MafujulHaquePlabon <mafujul15-3076@diu.edu.bd>
2022-04-05 05:12:17 +0600

    PieCharts added

--- src/components/PieCharts/PieCharts.js
+++ src/components/PieCharts/PieCharts.js
@@ -4,7 +4,14 @@ import { Pie, PieChart, Tooltip } from 'recharts';
 const PieCharts = ({dataItems}) => {
     return (
        <div className='my-4 '>
-            
+          <div className='my-4 '>
+            <h1 className="text-blue-700 font-semibold text-xl">Investment vs Revenue</h1>
+        <PieChart width={400} height={400}>
+        <Pie data={dataItems} dataKey="investment" cx="50%" cy="50%" outerRadius={60} fill="#8884d8" />
+        <Pie data={dataItems} dataKey="revenue" cx="50%" cy="50%" innerRadius={70} outerRadius={90} fill="#82ca9d" label />
+       
+      </PieChart>
+       </div>  
        </div>
     );
 };
